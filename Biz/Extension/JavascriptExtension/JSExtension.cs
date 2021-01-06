4133ab4ddbdf98255ce5169a7a11620ee8822f6e
muhmmdfrd <mfarid2121@gmail.com>
2021-01-06 19:57:00 +0700

    add for serializer and deserializer object

---
+++ Biz/Extension/JavascriptExtension/JSExtension.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System.Web.Script.Serialization;
+
+namespace Biz.Extension.JavascriptExtension
+{
+	public static class JSExtension
+	{
+		public static string Serialize(this object obj)
+		{
+			return new JavaScriptSerializer().Serialize(obj);
+		}
+
+		public static T Deserialize<T>(this string obj)
+		{
+			return JsonConvert.DeserializeObject<T>(obj);
+		}
+	}
+}
